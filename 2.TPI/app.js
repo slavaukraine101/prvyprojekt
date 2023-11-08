4b187ec0ef80c9efdb41b93891638ca3874c3c9f
egordonec95@gmail.com <egordonec95@gmail.com>
2023-11-08 13:43:51 +0100

    first commit

---
+++ 2.TPI/app.js
@@ -0,0 +1,11 @@
+//alert("vysledok");
+
+
+let prvaPremenna = 3;
+let druhaPremenna = 7;
+// let vysledok = 3+ 7;
+let vysledok = prvaPremenna + druhaPremenna;
+
+alert( 3 + 7);
+alert( prvaPremenna + druhaPremenna);
+alert(vysledok);
---
+++ 2.TPI/helper_script.js
@@ -0,0 +1,7 @@
+const currentOutput01 = document.getElementById("text_output01");
+const currentOutput02 = document.getElementById("text_output02");
+
+function vysledok(prvy, druhy) {
+  currentOutput01.textContent = prvy;
+  currentOutput02.textContent = druhy;
+}
